270999383171eb21a6772984428bad610b377ee8
douglas <douglas.arau@hotmail.com>
2023-10-04 18:18:03 -0300

    refazendo o modalExcluir começando pelo fetch

--- src/components/ModalExcluir/ModalExcluir.jsx
+++ src/components/ModalExcluir/ModalExcluir.jsx
@@ -3,38 +3,25 @@ import { useNavigate } from "react-router-dom";
 import "./ModalExcluir.scss";
 
 export default function ModalExcluir(props) {
+  const navigation = useNavigate();
+  const [idToDelete, setIdToDelete] = useState(""); // Estado para acompanhar o ID do produto a ser excluído.
 
-    const navigation = useNavigate();
-
-    const[produto, setProduto] = useState({
-      id:"",
-    });
-
-    const handleChange = (e)=>{
-      //Destructuring
-      const {name,value} = e.target;
-      //Inserir as propriedades no produto.
-      setProduto({...produto,[name]:value});
-    }
-
-    const handleSubmit = (e)=>{
-      e.preventDefault();
-      
-      fetch("http://localhost:5000/produtos",{
-        method:"DELETE",
-        headers:{
-          "Content-Type":"application/json"
-        },
-        body: JSON.stringify(produto)
+  const handleDelete = () => {
+    // Faça uma solicitação DELETE para o servidor com o ID a ser excluído.
+    fetch(`http://localhost:5000/produtos/${idToDelete}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response); // Verifique a resposta do servidor (pode ser uma mensagem de sucesso).
+        props.setOpen(false);
+        navigation("/produtos");
       })
-      .then((response)=> response.json())
-      .then((response)=> console.log(response))
-      .catch(error=> console.log(error));
-
-      //Fechando o modal
-      props.setOpen(false);
-      navigation("/produtos");
-    }
+      .catch((error) => console.error(error));
+  };
 
 
   if (props.open) {
